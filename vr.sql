49617a33b9031cea576a862c3c4f09199d268a6e
yangcheng <yang759126596@163.com>
2023-04-18 09:03:31 +0800

    Update vr.sql

--- vr.sql
+++ vr.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80032
  File Encoding         : 65001
 
- Date: 16/04/2023 09:18:19
+ Date: 18/04/2023 09:03:22
 */
 
 SET NAMES utf8mb4;
@@ -21,51 +21,47 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE `user`  (
   `id` int NOT NULL COMMENT '主键ID',
   `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '姓名',
-  `nickname` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '昵称',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `nickname` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '昵称',
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
-  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '头像',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像',
   `gender` tinyint(1) NOT NULL COMMENT '性别,0未知,1男,2女性别',
-  `birth` datetime DEFAULT NULL COMMENT '出生日期',
+  `birth` datetime NULL DEFAULT NULL COMMENT '出生日期',
   `is_enabled` tinyint(1) NOT NULL COMMENT '是否启用',
-  `create_by` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '创建人',
-  `created_time` datetime DEFAULT NULL COMMENT '创建日期',
-  `update_time` datetime DEFAULT NULL COMMENT '更新日期',
+  `create_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建人',
+  `created_time` datetime NULL DEFAULT NULL COMMENT '创建日期',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新日期',
   `delete_flag` tinyint(1) NOT NULL COMMENT '删除标记',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='用户';
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-BEGIN;
-COMMIT;
 
 -- ----------------------------
 -- Table structure for vr
 -- ----------------------------
 DROP TABLE IF EXISTS `vr`;
-CREATE TABLE `vr` (
+CREATE TABLE `vr`  (
   `id` int NOT NULL COMMENT '主键',
   `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
   `thumbnail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '缩略图',
-  `scenes` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '场景',
-  `rotate_speed` tinyint(1) DEFAULT NULL COMMENT '旋转速度,0慢,1中,2快',
-  `is_watermark` tinyint(1) DEFAULT NULL COMMENT '去水印,0否,1是',
-  `is_auto_rotate` tinyint(1) DEFAULT NULL COMMENT '自动旋转,0否,1是',
-  `is_automatic_jump` tinyint(1) DEFAULT NULL COMMENT '自动跳转,0否,1是',
-  `is_asteroid_opening` tinyint(1) DEFAULT NULL COMMENT '小行星开场,0否,1是',
-  `views` int DEFAULT NULL COMMENT '浏览量',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='全景';
+  `scenes` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '当前场景',
+  `rotate_speed` tinyint(1) NULL DEFAULT NULL COMMENT '旋转速度,0慢,1中,2快',
+  `is_watermark` tinyint(1) NULL DEFAULT NULL COMMENT '去水印,0否,1是',
+  `is_auto_rotate` tinyint(1) NULL DEFAULT NULL COMMENT '自动旋转,0否,1是',
+  `is_automatic_jump` tinyint(1) NULL DEFAULT NULL COMMENT '自动跳转,0否,1是',
+  `is_asteroid_opening` tinyint(1) NULL DEFAULT NULL COMMENT '小行星开场,0否,1是',
+  `views` int NULL DEFAULT NULL COMMENT '浏览量',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '全景' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of vr
 -- ----------------------------
-BEGIN;
-COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
